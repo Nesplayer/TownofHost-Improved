ba37554b7a3b40df6648a2b8c4cab99a545dd182
Polkasworn <lime19844@gmail.com>
2026-03-08 12:37:00 -0700

    remove redeemer bs & protector minor rework

--- Roles/Crewmate/Protector.cs
+++ Roles/Crewmate/Protector.cs
@@ -18,75 +18,53 @@ internal class Protector : RoleBase
     //==================================================================\\
 
     private static OptionItem MaxShields;
-    private static OptionItem ShieldDuration;
-    private static OptionItem ShieldIsOneTimeUse;
 
-    private long? TimeStamp;
 
     public override void SetupCustomOption()
     {
         Options.SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Protector);
         MaxShields = IntegerOptionItem.Create(Id + 10, "ProtectorMaxShields", new(1, 14, 1), 3, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Protector])
             .SetValueFormat(OptionFormat.Votes);
-        ShieldDuration = FloatOptionItem.Create(Id + 11, "ProtectorShieldDuration", new(1, 30, 1), 10, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Protector])
-            .SetValueFormat(OptionFormat.Seconds);
-        ShieldIsOneTimeUse = BooleanOptionItem.Create(Id + 12, "ShieldIsOneTimeUse", false, TabGroup.CrewmateRoles, false).SetParent(Options.CustomRoleSpawnChances[CustomRoles.Protector]);
         Options.OverrideTasksData.Create(Id + 13, TabGroup.CrewmateRoles, CustomRoles.Protector);
     }
     public override void Add(byte playerId)
     {
         playerId.SetAbilityUseLimit(MaxShields.GetInt());
-        TimeStamp = 0;
 
         if (!Main.ResetCamPlayerList.Contains(playerId))
             Main.ResetCamPlayerList.Add(playerId);
     }
-    public override void ReceiveRPC(MessageReader reader, PlayerControl NaN)
+    private Dictionary<byte, bool> ProtectorInProtect = [];
+
+    public override bool OnTaskComplete(PlayerControl player, int completedTaskCount, int totalTaskCount)
     {
-        byte PlayerId = reader.ReadByte();
-        string Time = reader.ReadString();
-        TimeStamp = long.Parse(Time);
+        if (player.GetAbilityUseLimit() <= 0) return true;
+        player.RpcRemoveAbilityUse();
+        ProtectorInProtect[player.PlayerId] = true;
+        return true;
     }
-    private bool ProtectorInProtect(byte playerId) => TimeStamp > Utils.GetTimeStamp();
 
     public override bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target)
     {
-        if (ProtectorInProtect(target.PlayerId))
+        if (ProtectorInProtect[target.PlayerId])
         {
             killer.RpcGuardAndKill(target);
             if (!DisableShieldAnimations.GetBool()) target.RpcGuardAndKill();
             target.Notify(GetString("ProtectorShield"));
-            if (ShieldIsOneTimeUse.GetBool())
-            {
-                TimeStamp = 0;
-                Logger.Info($"{target.GetNameWithRole()} shield broken", "ProtectorShieldBroken");
-            }
+            ProtectorInProtect[target.PlayerId] = false;
+            Logger.Info($"{target.GetNameWithRole()} shield broken", "ProtectorShieldBroken");
             return false;
         }
-        else if (killer.GetCustomRole() == target.GetCustomRole()) return false;
         return true;
-    }
-    public override void OnFixedUpdateLowLoad(PlayerControl pc)
-    {
-        if (TimeStamp < Utils.GetTimeStamp() && TimeStamp != 0)
-        {
-            TimeStamp = 0;
-            pc.Notify(GetString("ProtectorShieldBrokenOrEnded"), sendInLog: false);
-        }
     }
     public override string GetLowerText(PlayerControl pc, PlayerControl seen = null, bool isForMeeting = false, bool isForHud = false)
     {
         if (pc == null || isForMeeting || !isForHud || !pc.IsAlive()) return string.Empty;
 
         var str = new StringBuilder();
-        if (ProtectorInProtect(pc.PlayerId))
+        if (ProtectorInProtect[pc.PlayerId])
         {
-            var remainTime = TimeStamp - Utils.GetTimeStamp();
-            str.Append(string.Format(GetString("ProtectorSkillTimeRemain"), remainTime));
-        }
-        else
-        {
-            str.Append(GetString("ProtectorSkillNotice"));
+            str.Append(string.Format(GetString("ProtectorSkillTimeRemain")));
         }
         return str.ToString();
     }
